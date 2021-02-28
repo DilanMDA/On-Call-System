681601dcc05927f47631fa710974e1a75973a70f
DilanMDA <akiladilan04@gmail.com>
2021-02-28 16:17:38 +0530

    Firebase connected & Config

--- src/store/store.js
+++ src/store/store.js
@@ -1,17 +1,27 @@
-// global Redux Store
-
-import { createStore, applyMiddleware, compose } from "redux";
+import { compose, createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-import rootReducer from "./reducer/rootReducer";
+import firebase from "../Firebase/Firebase";
+import { reactReduxFirebase, getFirebase } from "react-redux-firebase";
+import { reduxFirestore, getFirestore } from "redux-firestore";
+import rootReducer from "./reducers/rootReducer";
+
+// react-redux-firebase config
+const rrfConfig = {
+  userProfile: "users",
+  useFirestoreForProfile: true, // Firestore for Profile instead of Realtime DB
+};
 
 const composeEnhancers =
   process.env.NODE_ENV === "development"
     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
     : compose;
-
 const store = createStore(
   rootReducer,
-  composeEnhancers(applyMiddleware(thunk))
+  composeEnhancers(
+    reactReduxFirebase(firebase, rrfConfig),
+    reduxFirestore(firebase),
+    applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore }))
+  )
 );
 
 export default store;
